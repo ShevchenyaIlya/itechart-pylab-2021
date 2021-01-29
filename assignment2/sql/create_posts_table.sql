1cd9d4e1c7ff3c8f5f7e4689c6ad2a27b4a42bc0
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-01-29 14:47:10 +0300

    Modify string fields to numeric.

--- assignment2/sql/create_posts_table.sql
+++ assignment2/sql/create_posts_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS posts(
     unique_id uuid UNIQUE NOT NULL,
     post_url TEXT,
     post_date DATE NOT NULL,
-    comments_number VARCHAR(10) NOT NULL,
-    votes_number VARCHAR(10) NOT NULL,
+    comments_number BIGINT NOT NULL,
+    votes_number BIGINT NOT NULL,
     post_category VARCHAR(20) NOT NULL,
     owner BIGSERIAL REFERENCES users(user_id));
--- assignment2/sql/create_users_table.sql
+++ assignment2/sql/create_users_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
     user_id BIGSERIAL PRIMARY KEY NOT NULL,
     username VARCHAR(50) UNIQUE,
-    user_karma VARCHAR(10) NOT NULL,
+    user_karma BIGINT NOT NULL,
     user_cake_day DATE NOT NULL,
-    post_karma VARCHAR(10) NOT NULL,
-    comment_karma VARCHAR(10) NOT NULL);
+    post_karma BIGINT NOT NULL,
+    comment_karma BIGINT NOT NULL);
