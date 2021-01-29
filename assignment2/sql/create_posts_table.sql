a5eda52ba1eec939d5ad2fb74068a3031fa3787a
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-01-29 14:04:06 +0300

    Update owner field type.

--- assignment2/sql/create_posts_table.sql
+++ assignment2/sql/create_posts_table.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS posts(
     comments_number VARCHAR(10) NOT NULL,
     votes_number VARCHAR(10) NOT NULL,
     post_category VARCHAR(20) NOT NULL,
-    owner SMALLSERIAL REFERENCES users(user_id));
+    owner BIGSERIAL REFERENCES users(user_id));
