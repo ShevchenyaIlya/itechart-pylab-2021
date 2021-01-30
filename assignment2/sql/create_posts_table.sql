51371beeca1cdce7001f5f018c53b5cfedccdd89
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-01-30 21:01:31 +0300

    Removed unnecessary files, and rename module.

--- assignment2/sql/create_posts_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS posts(
-    post_id BIGSERIAL PRIMARY KEY NOT NULL,
-    unique_id uuid UNIQUE NOT NULL,
-    post_url TEXT,
-    post_date DATE NOT NULL,
-    comments_number BIGINT NOT NULL,
-    votes_number BIGINT NOT NULL,
-    post_category VARCHAR(20) NOT NULL,
-    owner BIGSERIAL REFERENCES users(user_id));
--- assignment2/sql/create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-    user_id BIGSERIAL PRIMARY KEY NOT NULL,
-    username VARCHAR(50) UNIQUE,
-    user_karma BIGINT NOT NULL,
-    user_cake_day DATE NOT NULL,
-    post_karma BIGINT NOT NULL,
-    comment_karma BIGINT NOT NULL);
