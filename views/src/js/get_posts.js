45ac0e28edf124a0d58417152284aa00b8125cd9
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-07 23:19:02 +0300

    Asyncronous request to server from javascript for getting posts

---
+++ views/src/js/get_posts.js
@@ -0,0 +1,18 @@
+async function fetchPosts(filters = "") {
+    let url = 'http://localhost:8087/posts/'
+    if (filters !== "") {
+        url += "?" + filters;
+    }
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        async: true,
+    });
+
+    let response_data = await response;
+    if (response_data.status === 200) {
+        return response_data.json();
+    }
+
+    return [];
+}
