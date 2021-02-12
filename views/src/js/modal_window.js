a0629e1d674091339dbc3456f5294050c5e560ad
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-12 23:26:23 +0300

    Remove from modal window ordering and hew components.

--- views/src/js/modal_window.js
+++ views/src/js/modal_window.js
@@ -9,61 +9,23 @@ function showModalWindow() {
     }
 }
 
-class Swapper extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isASC: true};
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick()
-    {
-        this.setState(state => ({
-            isASC: !state.isASC
-        }));
-
-        let order_icon = document.getElementById("order-icon");
-        if (this.state.isASC) {
-            order_icon.className = "fa fa-angle-double-up";
-            configurations.order = "DESC";
-        }
-        else {
-            configurations.order = "ASC";
-            order_icon.className = "fa fa-angle-double-down";
-        }
-        updatePostsList()
-    }
-
-    render()
-    {
-        return (
-            <h4 onClick={this.handleClick}>
-                Available Filters <i id="order-icon" className="fa fa-angle-double-down" aria-hidden="true"></i>
-            </h4>
-        );
-    }
-}
-
-
 function FilterItem(props) {
     const filter = props.filter_name
+
     function handler() {
-        console.log(filter)
-        configurations.filter_field = filter;
+        configurations.sorting_field = filter;
         updatePostsList();
     }
 
-    return <p onClick={handler}>{props.filter_name}</p>;
+    return <p onClick={handler}>{filter}</p>;
 }
-
 
 function Filters(props) {
     const filters = props.filters;
 
     return (
         <div id="modal-window" style={{display: 'none'}}>
-            <Swapper />
+            <h4>Filters</h4>
             {
                 filters.map((filter) => <FilterItem key={filter} filter_name={filter}/>)
             }
