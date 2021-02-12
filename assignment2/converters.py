29b97ef907e2101e58e377d1496ea37d3577b918
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-12 21:16:54 +0300

    Parsing url using urllib and add converters for forming part of sql querys.

--- assignment2/converters.py
+++ assignment2/converters.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from urllib import parse
 
 from assignment2.validators import validate_url_parameters
 
@@ -55,13 +56,46 @@ def convert_post_field(karma: str) -> int:
 
 
 def parse_url_parameters(url_parameters: str):
-    url_parameters = url_parameters[url_parameters.index("?", 0) + 1 :]
-    filters = list(
-        map(lambda filter_pair: filter_pair.split("="), url_parameters.split("&"))
-    )
+    url_parameters = parse.urlsplit(url_parameters).query
+    filters = parse.parse_qs(url_parameters)
 
     return {
-        filter_pair[0]: filter_pair[1]
-        for filter_pair in filters
-        if len(filter_pair) == 2 and validate_url_parameters(filter_pair[0])
+        key: value[0] for key, value in filters.items() if validate_url_parameters(key)
     }
+
+
+def get_selected_filters(filters: dict, condition: list):
+    return {key: value for key, value in filters.items() if key in condition}
+
+
+def form_order_string(filters):
+    filters = get_selected_filters(filters, ["sorting_field", "order"])
+    if not filters.get("sorting_field", False):
+        return ""
+
+    return " ".join(["ORDER BY", filters["sorting_field"], filters.get("order", "ASC")])
+
+
+def form_condition_string(filters: dict):
+    rules = {
+        "post_category": "post_category='%s'",
+        "post_date": "post_date::date='%s'",
+        "votes_number": "(votes_number >= %s AND votes_number <= %d)",
+    }
+    filters = get_selected_filters(
+        filters, ["post_category", "votes_number", "post_date"]
+    )
+    if not filters:
+        return ""
+
+    conditions = []
+    for key, value in filters.items():
+        if key != "votes_number":
+            conditions.append(rules[key].replace("%s", value))
+        else:
+            value = value.split("-")
+            conditions.append(
+                rules[key].replace("%s", value[0]).replace("%d", value[1])
+            )
+
+    return "WHERE " + " AND ".join(conditions)
