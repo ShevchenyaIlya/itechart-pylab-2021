056be68c998e181436b2fafc36801213b4ef33a0
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-12 21:15:37 +0300

    Extend url components validation logic

--- assignment2/validators.py
+++ assignment2/validators.py
@@ -1,16 +1,52 @@
+from datetime import datetime
+
+
 def validate_url_parameters(filter_name: str):
-    possible_filters = ["page", "filter_field", "order"]
+    possible_filters = [
+        "page",
+        "post_category",
+        "post_date",
+        "votes_number",
+        "sorting_field",
+        "order",
+    ]
+
     return filter_name in possible_filters
+
+
+def sorting_field_validation(value: str):
+    return value in ["post_category", "votes_number", "post_date"]
 
 
 def page_number_validation(value: str):
     return value.isdigit()
 
 
-def filter_fields_validation(value: str):
-    possible_filters = ["post_date", "post_category", "votes_number"]
+def category_validation(value: str, categories: list):
+    if any(isinstance(category, tuple) for category in categories):
+        contained = (value,) in categories
+    else:
+        contained = value in categories
 
-    return value in possible_filters
+    return contained
+
+
+def post_date_validation(value: str):
+    try:
+        datetime.strptime(value, "%Y-%m-%d")
+    except ValueError:
+        return False
+
+    return True
+
+
+def votes_number_validation(value: str):
+    borders = value.split("-")
+
+    if all([number.isdigit() for number in borders]):
+        return borders[0] < borders[1]
+
+    return False
 
 
 def order_validation(value: str):
@@ -19,13 +55,22 @@ def order_validation(value: str):
     return value in possible_orders
 
 
-def validate_url_parameters_values(parameter_pairs: dict):
+def validate_url_parameters_values(parameter_pairs: dict, categories):
     validator = {
         "page": page_number_validation,
-        "filter_field": filter_fields_validation,
+        "post_category": category_validation,
+        "post_date": post_date_validation,
+        "votes_number": votes_number_validation,
+        "sorting_field": sorting_field_validation,
         "order": order_validation,
     }
 
     for key in list(parameter_pairs):
-        if not validator[key](parameter_pairs[key]):
+        is_valid = (
+            validator[key](parameter_pairs[key], categories)
+            if key == "post_category"
+            else validator[key](parameter_pairs[key])
+        )
+
+        if not is_valid:
             parameter_pairs.pop(key)
