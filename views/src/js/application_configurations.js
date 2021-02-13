ba195a4591f6a63df2deea243aac1108ad3de116
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-13 20:03:19 +0300

    Extend configuration for work with filters.

--- views/src/js/application_configurations.js
+++ views/src/js/application_configurations.js
@@ -1,9 +1,12 @@
 let configurations = {
-    filter_field: "post_category",
+    sorting_field: "post_category",
+    post_category: null,
+    post_date: null,
+    votes_number: null,
     order: "ASC",
     page: 0,
 }
-const filters = ["post_category", "post date", "votes_number"]
+const filters = ["post_category", "post_date", "votes_number"]
 const postsContainer = document.getElementById("posts_list");
 
 
@@ -11,7 +14,9 @@ function createFiltersLine(configuration) {
     let filtersLine = [];
 
     for (const [key, value] of Object.entries(configuration)) {
-        filtersLine.push(`${key}=${value}`);
+        if (value != null && value !== "") {
+            filtersLine.push(`${key}=${value}`);
+        }
     }
 
     return filtersLine.join("&");
