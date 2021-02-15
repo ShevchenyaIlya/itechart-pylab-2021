6e68add6bf5dd1581794cf51fb0fa6ace870f467
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-15 15:45:25 +0300

    Change sql query for selecting posts with filters.

--- assignment2/sql/select_all_posts_with_filters.sql
+++ assignment2/sql/select_all_posts_with_filters.sql
@@ -1,4 +1,8 @@
 SELECT * FROM posts INNER JOIN users ON (posts.owner=users.user_id)
-%s
-%s
+WHERE post_category=COALESCE(%s, post_category) AND
+      post_date=COALESCE(%s, post_date) AND (
+        votes_number >= COALESCE(%s, votes_number) AND
+        votes_number <= COALESCE(%s, votes_number)
+     )
+ORDER BY %s %s
 LIMIT %s OFFSET %s;
