c959bbfcaf24c43d2449924ddd9f71b2a9f58cb2
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-16 12:59:43 +0300

    Change sql querys formatting

--- assignment2/sql/insert_post.sql
+++ assignment2/sql/insert_post.sql
@@ -1,2 +1,8 @@
-INSERT INTO posts(unique_id, post_url, post_date, comments_number, votes_number, post_category, owner)
-VALUES(%s, %s, %s, %s, %s, (SELECT category_id from categories WHERE LOWER(category_name)=LOWER(%s)), (SELECT user_id from users WHERE username=%s));
+INSERT INTO posts(
+    unique_id, post_url, post_date, comments_number, votes_number, post_category, owner
+)
+VALUES (
+    %s, %s, %s, %s, %s,
+    (SELECT category_id from categories WHERE LOWER(category_name)=LOWER(%s)),
+    (SELECT user_id from users WHERE username=%s)
+);
