5f6277fda44a370efeee50f30ef0209a9c3639fd
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-24 20:04:48 +0300

    Add validator for roles

--- document_collaboration_system/backend/role.py
+++ document_collaboration_system/backend/role.py
@@ -1,4 +1,11 @@
+from typing import List
+
+
 class Role:
     LAWYER = "Lawyer"
     ECONOMIST = "Economist"
-    GENERAL_DIRECTOR = "General director "
+    GENERAL_DIRECTOR = "General director"
+
+    @classmethod
+    def get_roles(cls) -> List[str]:
+        return [cls.LAWYER, cls.ECONOMIST, cls.GENERAL_DIRECTOR]
---
+++ document_collaboration_system/backend/validators.py
@@ -0,0 +1,5 @@
+from role import Role
+
+
+def role_validation(role_name: str) -> bool:
+    return role_name in Role.get_roles()
