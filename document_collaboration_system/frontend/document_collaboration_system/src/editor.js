13e0b2945c9cf622454334bb77d1bd1ee6d985c3
ShevchenyaIlya <shevchenya.i@gmail.com>
2021-02-26 00:28:58 +0300

    Add comment dialog and custom buttom with action in editor tools menu

--- document_collaboration_system/frontend/document_collaboration_system/src/editor.js
+++ document_collaboration_system/frontend/document_collaboration_system/src/editor.js
@@ -1,11 +1,44 @@
-import React, {Component} from "react";
-import {convertToRaw, convertFromRaw, EditorState} from "draft-js";
+import React, {Component, useState} from "react";
+import {convertToRaw, convertFromRaw, EditorState, Modifier} from "draft-js";
+import PropTypes from 'prop-types';
 import {Editor} from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import {Paper} from "@material-ui/core";
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import {send_request} from "./send_request";
 import {useHistory} from "react-router-dom";
 import CustomMenu from "./menu";
+import FormDialog from "./placeCommentDialog";
+
+const styleMap = {
+  'COMMENT': {
+    backgroundColor: 'rgba(255, 255, 0, 0.5)',
+  },
+};
+
+class CustomOption extends Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+    editorState: PropTypes.object,
+  };
+
+  addComment = () => {
+    this.props.setOpen(true);
+    const { editorState, onChange } = this.props;
+    const contentState = Modifier.applyInlineStyle(
+      editorState.getCurrentContent(),
+      editorState.getSelection(),
+      'COMMENT',
+    );
+    onChange(EditorState.push(editorState, contentState, 'change-inline-style'));
+  };
+
+  render() {
+    return (
+      <div id="customEditorButton" onClick={this.addComment}><ChatBubbleIcon/></div>
+    );
+  }
+}
 
 class ControlledEditor extends Component {
   constructor(props) {
@@ -22,6 +55,10 @@ class ControlledEditor extends Component {
           this.props.history.push("/");
         }
         else if (Object.keys(content.content).length !== 0) {
+          if (content.status === "Archive") {
+            this.props.setMode(true);
+          }
+
           const DBEditorState = convertFromRaw(content.content);
           const local_state = EditorState.createWithContent(DBEditorState);
           this.setState({
@@ -63,18 +100,25 @@ class ControlledEditor extends Component {
           wrapperClassName="demo-wrapper"
           editorClassName="demo-editor"
           onEditorStateChange={this.onEditorStateChange}
+          readOnly={this.props.readOnly}
+          toolbarCustomButtons={[<CustomOption setOpen={this.props.setOpen}/>]}
+          customStyleMap={styleMap}
         />
     );
   }
 }
 
 function DocumentEditor({document}) {
+  let [readOnlyDocument, setMode] = useState(false);
+  const [openModalWindow, setOpen] = React.useState(false);
   const history = useHistory();
+
   return (
       <>
-        <CustomMenu document={document}/>
+        <FormDialog openModalWindow={openModalWindow} setOpen={setOpen}/>
+        <CustomMenu document={document} readOnly={readOnlyDocument} setMode={setMode}/>
         <Paper elevation={3} className="editorContainer">
-          <ControlledEditor document={document} history={history}/>
+          <ControlledEditor document={document} history={history} readOnly={readOnlyDocument} setMode={setMode} setOpen={setOpen}/>
         </Paper>
       </>
   );
